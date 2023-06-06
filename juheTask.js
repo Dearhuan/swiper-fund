65b04d59a2930c54eac5777a8fe692daffce0aa4
HeHuan <clearhuan@qq.com>
2023-06-06 10:39:32 +0800

    date format fix

--- juheTask.js
+++ juheTask.js
@@ -77,11 +77,11 @@ const dateFormater = (formater, time) => {
     s = date.getSeconds();
   return formater.replace(/YYYY|yyyy/g, Y)
     .replace(/YY|yy/g, Y.substr(2, 2))
-    .replace(/MM/g, (M < 10 ? '0' : '') + M)
-    .replace(/DD/g, (D < 10 ? '0' : '') + D)
-    .replace(/HH|hh/g, (H < 10 ? '0' : '') + H)
-    .replace(/mm/g, (m < 10 ? '0' : '') + m)
-    .replace(/ss/g, (s < 10 ? '0' : '') + s)
+    .replace(/MM/g, M)
+    .replace(/DD/g, D)
+    .replace(/HH|hh/g, H)
+    .replace(/mm/g, m)
+    .replace(/ss/g, s)
 }
 
 const randomNum = (min, max) => Math.floor(Math.random() * (max - min + 1)) + min;
